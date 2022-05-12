d5ce1b0e8701bbb6ef70d81c542d18a1072370f6
SentiWW <sentiww@gmail.com>
2022-05-12 15:03:27 +0200

    Add Creature, Human, and Attributes class that model creatures and humans with their basic attributes

---
+++ AutoBattlerSharp.Logic/Models/Attributes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoBattlerSharp.Logic.Models
+{
+    internal class Attributes
+    {
+        public byte Melee { get; set; }
+        public byte Range { get; set; }
+        public byte Sturdiness { get; set; }
+        public byte Resistance { get; set; }
+        public byte Agility { get; set; }
+        public byte Intelligence { get; set; }
+        public byte Attacks { get; set; }
+        public byte Health { get; set; }
+        public byte Speed { get; set; }
+        public byte Strength { get; set; }
+        public byte Magic { get; set; }
+    }
+}
--- AutoBattlerSharp.Logic/Models/Entity.cs
+++ AutoBattlerSharp.Logic/Models/Entity.cs
@@ -1,12 +1,14 @@
-﻿namespace AutoBattlerSharp.Logic
+﻿namespace AutoBattlerSharp.Logic.Models
 {
-    /// <summary>
-    /// 
-    /// </summary>
     public abstract class Entity
     {
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+
+        public Entity(string name, string description)
+        {
+            (Id, Name, Description) = (Guid.NewGuid(), name, description);
+        }
     }
 }
