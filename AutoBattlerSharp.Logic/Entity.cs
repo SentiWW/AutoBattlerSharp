eebec0df46f28b7323b7467039c8bf26937f4b46
SentiWW <sentiww@gmail.com>
2022-05-12 14:26:53 +0200

    Add project files.

---
+++ AutoBattlerSharp.Logic/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace AutoBattlerSharp.Logic
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ AutoBattlerSharp/MainWindow.cs
@@ -0,0 +1,10 @@
+namespace AutoBattlerSharp
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
