18a30290e8c55dc828375573dfe1ab13b15a2ced
NotImplementedUsername <105523062+NotImplementedUsername@users.noreply.github.com>
2022-05-14 14:55:13 +0200

    Add files via upload
    The extension creates the basis for adding items

---
+++ AutoBattlerSharp.Logic/Models/IWeapon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoBattlerSharp.Logic.Models
+{
+    public interface IWeapon
+    {
+        public byte AttackPoints { get; set; }
+        public float Accuracy { get; set; }
+
+        public byte GetAttackDamage(byte attack, byte magic, byte sturdiness, byte strength, byte intelligence);
+
+    }
+}
