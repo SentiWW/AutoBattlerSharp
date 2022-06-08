6d665f9af13a66c6b012bca10035f7820ca6631b
NotImplementedUsername <spam1234@o2.pl>
2022-06-08 13:15:59 +0200

    New armours types added

---
+++ AutoBattlerSharp.Logic/Models/Items/Armours/Clothes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace AutoBattlerSharp.Logic.Models.Items.Armours
+{
+    public class Clothes : Armour, IArmour
+    {
+        public Clothes(string name, string description, short value, short weight, short defencePoints)
+            : base(name, description, value, weight, defencePoints)
+        {
+
+        }
+
+        [JsonConstructor]
+        public Clothes()
+        {
+
+        }
+
+        public new short GetDefence(Attributes attributes)
+        {
+            return (short)(base.GetDefence(attributes) + attributes.Resistance);
+        }
+
+    }
+}
