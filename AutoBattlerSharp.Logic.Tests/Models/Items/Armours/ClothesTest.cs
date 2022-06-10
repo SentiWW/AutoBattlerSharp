8094835e61701b048971071509885e9d31a9916c
NotImplementedUsername <spam1234@o2.pl>
2022-06-10 13:41:51 +0200

    First test for items

---
+++ AutoBattlerSharp.Logic.Tests/Models/Items/Armours/ClothesTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using AutoBattlerSharp.Logic.Models;
+using AutoBattlerSharp.Logic.Models.Items;
+using AutoBattlerSharp.Logic.Models.Items.Armours;
+
+namespace AutoBattlerSharp.Logic.Tests.Models.Items.Armours
+{
+    public class ClothesTest
+    {
+        [Fact]
+        public void GetDefenceTest()
+        {
+            Attributes attributes = new Attributes();
+            attributes.Resistance = 5;
+            
+            IArmour armour = new Clothes("Clothes", "Test Clothes", 10, 5, 2);
+            
+            Assert.Equal(7, armour.GetDefence(attributes));
+        }
+
+    }
+}
---
+++ AutoBattlerSharp.Logic.Tests/Models/Items/Weapons/SwordTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using AutoBattlerSharp.Logic.Models;
+
+
+namespace AutoBattlerSharp.Logic.Tests.Models.Items.Weapons
+{
+    public class SwordTest
+    {
+        //[Fact]
+        public void GetAttackDamageTest()
+        {
+            
+        
+        }
+
+    }
+}
